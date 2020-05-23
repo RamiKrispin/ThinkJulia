4b4fa6a3fb292bce0e5d2f06cba3bcd6dfb679ca
RamiKrispin <ramkrisp@umich.edu>
2020-05-23 10:04:58 -0700

    update chapter 4

--- chapter 4.jl
+++ chapter 4.jl
@@ -15,3 +15,80 @@ end
 @svg begin
     square(🐢)
 end
+
+# Exercise 4-4
+function square(t, len)
+    for i in 1:4
+        forward(t, len)
+        turn(t, -90)
+    end
+end
+
+🐢= Turtle()
+
+@svg begin
+    square(🐢, 100)
+end
+
+
+# Exercise 4-5
+function polygon(t, n, len)
+    for i in 1:n
+        forward(t, len)
+        turn(t, -360/n)
+    end
+end
+
+🐢= Turtle()
+
+@svg begin
+    polygon(🐢, 100, 8)
+end
+
+# Exercise 4-6
+function circle(t, r)
+    circumference = 2 * pi * r
+    n = 50 # number of segment
+    len = circumference / n
+    polygon(t, len, n)
+end
+
+function circle(t, r)
+    circumference = 2 * pi * r
+    n = trunc(circumference / 3) + 3 # number of segment
+    len = circumference / n
+    polygon(t, len, n)
+end
+
+
+🐢= Turtle()
+
+@svg begin
+    circle(🐢, 100)
+end
+
+# Exercise 4-6
+
+"""
+polyline function - draw line
+"""
+function polyline(t, n, len, angle)
+    for i in 1:n
+        forward(t, len)
+        turn(t, -angle)
+    end
+end
+function arc(t, r, angle)
+    arc_len = 2 * pi * r * angle / 360
+    n = trunc(arc_len / 3) + 1
+    step_len = arc_len / n
+    step_angle = arc_len / n
+    polyline(t, n, step_len, step_angle)
+end
+
+
+🐢= Turtle()
+
+@svg begin
+    arc(🐢, 100, 180)
+end
