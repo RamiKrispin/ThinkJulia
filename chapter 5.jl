6f13eeb39f9a9df9c0c16d49943a2a9861ce505b
RamiKrispin <ramkrisp@umich.edu>
2020-05-23 16:48:50 -0700

    update chapter 5

---
+++ chapter 5.jl
@@ -0,0 +1,20 @@
+# Chapter 5
+# Floor division operator - ÷ (\div TAB)
+13 ÷ 2
+# Remainder %
+13 % 2
+
+# Boolean expressions - true / false
+typeof(true)
+typeof(false)
+
+# Relational operators
+x == y # Equal
+x != y # x is not equal to y
+x ≠ y # x is not equal to y - ≠ (\ne TAB)
+x > y # x greater than y
+x < y # x smaller than y
+x >= y # x greater or equal than y
+x ≥ y # x greater or equal than y - ≥ (\ge TAB)
+x <= y # x is less or equal than y
+x ≤ y # x is less or equal than y - ≤ (\le TAB)
