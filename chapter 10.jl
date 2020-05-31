1af410ab9f33c05c41dc55c0ebe8420776313776
RamiKrispin <ramkrisp@umich.edu>
2020-05-30 23:48:04 -0700

    update chapter 10

--- chapter 10.jl
+++ chapter 10.jl
@@ -7,3 +7,29 @@ x = [1, 2, 3]
 y = [1, "two", 3, 4]
 
 # Mutable
+y[2] = 2
+y
+2 ∈ y
+
+# Index
+eachindex(y)
+y[3:end]
+
+# Add elements
+
+push!(y, 5)
+t = [6, 7]
+append!(t, y)
+y
+t
+
+sort!(t)
+# sort (without !) will just print the sorted elements but won't change the order
+z = [1, 2, 5, 4, 3]
+sort(z)
+z
+
+# Summing the arrays
+
+t = [1, 2, 3, 4]
+sum(t)
